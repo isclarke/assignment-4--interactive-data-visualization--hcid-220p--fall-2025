70dd7bb4fc8f2139ccdf514360ef9d430e5e39cc
isclarke <isclarke@hartford.edu>
2025-12-16 19:30:00 -0500

    add legend format, and legend items

--- 5.3--legend/main.js
+++ 5.3--legend/main.js
@@ -1,4 +1,3 @@
-
 const width = 800;
 const height = 400;
 
@@ -8,7 +7,6 @@ const innerHeight = height - margin.top - margin.bottom;
 
 const drawStackedBars = (dataset) => {
 
-
   const svg = d3.select("#bars")
     .append("svg")
     .attr("viewBox", `0 0 ${width} ${height}`);
@@ -16,18 +14,21 @@ const drawStackedBars = (dataset) => {
   const chartGroup = svg.append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  const formats = [
-    "vinyl",
-    "eight_track",
-    "cassette",
-    "cd",
-    "download",
-    "streaming",
-    "other"
+//Legend Info
+  const formatsInfo = [
+    { key: "vinyl", label: "Vinyl" },
+    { key: "eight_track", label: "8-Track" },
+    { key: "cassette", label: "Cassette" },
+    { key: "cd", label: "CD" },
+    { key: "download", label: "Download" },
+    { key: "streaming", label: "Streaming" },
+    { key: "other", label: "Other" }
   ];
 
+  const formatKeys = formatsInfo.map(d => d.key);
+
   const colorScale = d3.scaleOrdinal()
-    .domain(formats)
+    .domain(formatKeys)
     .range(d3.schemeTableau10);
 
   const xScale = d3.scaleBand()
@@ -35,13 +36,13 @@ const drawStackedBars = (dataset) => {
     .range([0, innerWidth])
     .padding(0.15);
 
-  // Percent-based scale (0–100%)
   const yScale = d3.scaleLinear()
     .domain([0, 1])
     .range([innerHeight, 0]);
 
+//Stack Layout
   const stack = d3.stack()
-    .keys(formats)
+    .keys(formatKeys)
     .order(d3.stackOrderNone)
     .offset(d3.stackOffsetExpand);
 
@@ -73,6 +74,10 @@ const drawStackedBars = (dataset) => {
     .attr("transform", `translate(0, ${innerHeight})`)
     .call(xAxis)
     .selectAll("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-45)")
+    .attr("dx", "-0.6em")
+    .attr("dy", "0.25em");
 
   const yAxis = d3.axisLeft(yScale)
     .tickValues(d3.range(0, 1.01, 0.1))
@@ -81,6 +86,25 @@ const drawStackedBars = (dataset) => {
   chartGroup.append("g")
     .attr("class", "axis y-axis")
     .call(yAxis);
+
+//Legend
+  const legendItems = d3.select(".legend-container")
+    .append("ul")                       // #1
+      .attr("class", "color-legend")
+    .selectAll(".color-legend-item")
+    .data(formatsInfo)
+    .join("li")
+      .attr("class", "color-legend-item");
+
+  legendItems                            // #2
+    .append("span")
+      .attr("class", "color-legend-item-color")
+      .style("background-color", d => colorScale(d.key));
+
+  legendItems                            // #3
+    .append("span")
+      .attr("class", "color-legend-item-label")
+      .text(d => d.label);
 };
 
 d3.csv("/data/data.csv", d3.autoType)
